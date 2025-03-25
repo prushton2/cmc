d3e49c41f5a8aaefc065a8bac985d6eb878ea38d
prushton2 <peterrushton418@gmail.com>
2025-03-25 17:02:43 -0400

    added basic decimal and float constants

--- main.go
+++ main.go
@@ -3,6 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
-	parseNextToken()
+	s := "int main 0.5 500 5.21"
+
+	token, index := parseNextToken(s, 0)
+
+	fmt.Println(token)
+	fmt.Println(index)
+
+	for token.value != "" {
+		token, index = parseNextToken(s, index)
+		
+		fmt.Println("Parsed Token", token, "and got new starting index of", index)
+	}
+
 }
--- tokenizer.go
+++ tokenizer.go
@@ -4,11 +4,17 @@ import "fmt"
 import "regexp"
 
 const (
-	keyword       = 0
-	identifier    = 1
-	constant      = 2
-	stringliteral = 3
-	punctuator    = 4
+	KEYWORD        = 0
+	IDENTIFIER     = 1
+
+	FLOATCONSTANT  = 2
+	INTCONSTANT    = 3
+	ENUMCONSTANT   = 4
+	CHARCONSTANT   = 5
+	PREDEFCONSTANT = 6
+
+	STRINGLITERAL  = 7
+	PUNCTUATOR     = 8
 )
 
 
@@ -17,18 +23,53 @@ type Token struct {
 	tokenType int
 }
 
-func parseNextToken() {
-	var s = "int a = 2;"
+func parseNextToken(s string, startIndex int) (Token, int) {
+	s = s[startIndex:]
 
-	fmt.Println("Tokenizing")
+	fmt.Println("Parsing", s)
 
 	var keyword = regexp.MustCompile("(alignas|alignof|auto|bool|break|case|char|const|constexpr|continue|default|do|double|else|enum|extern|false|float|for|goto|if|inline|int|long|nullptr|register|restrict|return|short|signed|sizeof|static|static_assert|struct|switch|thread_local|true|typedef|typeof|typeof_unqual|union|unsigned|void|volatile|while|_Atomic|_BitInt|_Complex|_Decimal128|_Decimal32|_Decimal64|_Generic|_Imaginary|_Noreturn)([^a-zA-Z0-9])")
+	var identifier = regexp.MustCompile("([a-zA-Z]|_)([a-zA-Z0-9]|_)*")
+	var floatconstant = regexp.MustCompile("[0-9]*\\.[0-9]*")
+	var intconstant = regexp.MustCompile("[1-9][0-9]*")
 
-	// fmt.Println(exp)
-
+	for s[0] == ' ' || s[0] == '\n' { //destroy any whitespace between tokens
+		s = s[1:]
+		startIndex++;
+	}
+	
+	var token = Token{tokenType: 0, value: ""}
+	
 	var find = keyword.FindAllIndex([]byte(s), 1)
 
 	if(find != nil && find[0][0] == 0) {
-		token := Token{tokenType: 1, value: ""}
+		token = Token{tokenType: KEYWORD, value: s[find[0][0]:find[0][1]-1]}
+		return token, startIndex + find[0][1] - find[0][0] - 1;
 	}
+
+	find = identifier.FindAllIndex([]byte(s), 1)
+
+	if(find != nil && find[0][0] == 0) {
+		token = Token{tokenType: IDENTIFIER, value: s[find[0][0]:find[0][1]]}
+		fmt.Println("AAA", find[0][0], find[0][1])
+		return token, startIndex + find[0][1] - find[0][0];
+	}
+
+	find = floatconstant.FindAllIndex([]byte(s), 1)
+
+	if(find != nil && find[0][0] == 0) {
+		token = Token{tokenType: FLOATCONSTANT, value: s[find[0][0]:find[0][1]]}
+		return token, startIndex + find[0][1] - find[0][0];
+	}
+
+	find = intconstant.FindAllIndex([]byte(s), 1)
+
+	if(find != nil && find[0][0] == 0) {
+		token = Token{tokenType: INTCONSTANT, value: s[find[0][0]:find[0][1]]}
+		return token, startIndex + find[0][1] - find[0][0];
+	}
+
+	// fmt.Println(token)
+
+	return token, startIndex;
 }
