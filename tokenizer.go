63d4eece5245dc18bd6367255533c1a66cea3f82
prushton2 <peterrushton418@gmail.com>
2025-03-25 02:11:02 -0400

    makes token

--- tokenizer.go
+++ tokenizer.go
@@ -4,11 +4,11 @@ import "fmt"
 import "regexp"
 
 const (
-	keyword
-	identifier
-	constant
-	stringliteral
-	punctuator
+	keyword       = 0
+	identifier    = 1
+	constant      = 2
+	stringliteral = 3
+	punctuator    = 4
 )
 
 
@@ -18,21 +18,17 @@ type Token struct {
 }
 
 func parseNextToken() {
-	fmt.Println("Tokenizing")
-
-	exp, err := regexp.Compile("(int)([^a-zA-Z0-9])")
 	var s = "int a = 2;"
 
-	fmt.Println(exp)
-	fmt.Println(err)
+	fmt.Println("Tokenizing")
 
-	var find = exp.FindAllIndex([]byte(s), 1)
+	var keyword = regexp.MustCompile("(alignas|alignof|auto|bool|break|case|char|const|constexpr|continue|default|do|double|else|enum|extern|false|float|for|goto|if|inline|int|long|nullptr|register|restrict|return|short|signed|sizeof|static|static_assert|struct|switch|thread_local|true|typedef|typeof|typeof_unqual|union|unsigned|void|volatile|while|_Atomic|_BitInt|_Complex|_Decimal128|_Decimal32|_Decimal64|_Generic|_Imaginary|_Noreturn)([^a-zA-Z0-9])")
+
+	// fmt.Println(exp)
+
+	var find = keyword.FindAllIndex([]byte(s), 1)
 
 	if(find != nil && find[0][0] == 0) {
-		fmt.Println("Matched!!")
-		fmt.Println(s[find[0][0]:find[0][1]])
+		token := Token{tokenType: 1, value: ""}
 	}
-
-	// fmt.Println([0:2])
-
 }
