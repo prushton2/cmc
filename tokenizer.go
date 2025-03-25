0484ec92bb8497be63843242b92152cc25810099
prushton2 <peterrushton418@gmail.com>
2025-03-25 01:36:39 -0400

    regex

--- tokenizer.go
+++ tokenizer.go
@@ -3,14 +3,36 @@ package main
 import "fmt"
 import "regexp"
 
+const (
+	keyword
+	identifier
+	constant
+	stringliteral
+	punctuator
+)
+
+
+type Token struct {
+	value string
+	tokenType int
+}
+
 func parseNextToken() {
 	fmt.Println("Tokenizing")
 
-	exp, err := regexp.Compile("int(?!([a-zA-Z0-9]))");
+	exp, err := regexp.Compile("(int)([^a-zA-Z0-9])")
+	var s = "int a = 2;"
 
 	fmt.Println(exp)
 	fmt.Println(err)
 
-	fmt.Println(exp.Find([]byte(`int a = 2`)))
+	var find = exp.FindAllIndex([]byte(s), 1)
+
+	if(find != nil && find[0][0] == 0) {
+		fmt.Println("Matched!!")
+		fmt.Println(s[find[0][0]:find[0][1]])
+	}
+
+	// fmt.Println([0:2])
 
 }
